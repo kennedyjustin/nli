4f322e3569c21cb252e969cc79c8d7102b924b06
Justin Kennedy <46265478+kennedyjustin@users.noreply.github.com>
2023-05-26 02:23:20 +0000

    Plan

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 """Natural Language Interface"""
+import json
 import sys
 
 import openai
@@ -13,33 +14,91 @@ def completion(messages):
     return chat_completion.choices[0].message.content
 
 
+def get_output_type(arg):
+    messages = [
+        {
+            "role": "system",
+            "content": "A user will input a task/question. You must classify this request by providing the type of output that would best fit. Options: 'text', 'graph', 'spreadsheet', 'form', 'cron', 'empty'",
+        },
+        {"role": "user", "content": "Give me a report of last week's earnings"},
+        {"role": "user", "content": "spreadsheet"},
+        {"role": "user", "content": arg},
+    ]
+    return completion(messages)
+
+
+def create_plan(arg, output_type):
+    # TODO: JSON PLAN
+    messages = [
+        {
+            "role": "system",
+            "content": "A user will input a task/question. You must classify this request by providing the type of output that would best fit. Options: 'text', 'graph', 'spreadsheet', 'form', 'cron', 'empty'",
+        },
+        {"role": "user", "content": "Give me a report of last week's earnings"},
+        {"role": "user", "content": "spreadsheet"},
+        {"role": "user", "content": arg},
+    ]
+    return json.loads(completion(messages))
+
+
+def get_required_data_sources(plan):
+    return True
+
+
+def get_existing_data_sources():
+    return True
+
+
+def prompt_user_for_data_sources():
+    return True
+
+
+def verify_plan(plan):
+    return True
+
+
+def execute_plan(plan):
+    return True
+
+
+def save_result(result):
+    return True
+
+
 def prompt(arg):
     """prompt"""
-    print(arg)
-    # system, user, or assistant
-    messages = [{"role": "user", "content": arg}]
 
-    print(completion(messages))
+    output_type = get_output_type(arg)
+    print(f"output_type {output_type}")
+
+    # plan = create_plan(arg, output_type)
+    # required_data_sources = get_required_data_sources(plan)
+    # existing_data_sources = get_existing_data_sources()
+    # needed_data_sources = required_data_sources.difference(existing_data_sources)
+    # prompt_user_for_data_sources(needed_data_sources)
+    # existing_data_sources = get_existing_data_sources()
+    # verify_plan(plan)
+    # result = execute_plan(plan)
+    # save_result(result)
+    # return result
 
 
-# 1. Classification: Is prompt ephemeral or persistent (store it forever?)
 # 2. Classification: What is the output type?
-# 3. Classification: Is this a simple task/question, or will a larger plan be needed?
-# 4. Planning: Create a multi-step plan to complete this task
+# 3. Planning: Create a multi-step plan to complete this task
 #   - Include as context existing external and internal data sources
 #   - Include as context unconfigured potential external data sources
 #   - Ensure each step has a defined input and output (file, function return, etc.)
 #   - Steps can prompt additional input from the user
 #   - Specifically ensure that the final task has the proper desired output
-# 5. Classification: Given the plan, determine if an external data source needs to be configured
+# 4. Classification: Given the plan, determine if an external data source needs to be configured
 #   - Prompt the user if necessary using external data source instructions
-# 6. Classification: Verify the plan looks correct
-# 7. Execution: Step by step, execute the plan:
+# 5. Classification: Verify the plan looks correct
+# 6. Execution: Step by step, execute the plan:
 #   - Always provide context as to the larger plan
 #   - Always provide context on how to use data sources
 #       - Eventually give better context using embeddings / vector db
 #   - After each step, validate the output is correct
-# 8. Completion
+# 7. Completion
 #   - If output is ephemeral, simply return information back to the frontend
 #   - If output is persistent, save to a file if not already depending on output type
 #   - Record information in layout.json
